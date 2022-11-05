cbdff45fc69d1cee785545e727280ea940ff6b03
echo bolt <nikbal044@gmail.com>
2022-11-05 13:31:25 +0300

    Добавили my_superfunction

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def my_superfunction():
+    print("Много принтов")
 def main():
     print("Первый принт")
     print("Второй принт")
     print("Ещё принт")
     print("Заметки в поезде кайфуем")
-
+    my_superfunction()
 if __name__ == '__main__':
     main()
